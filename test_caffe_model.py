b347c4f4bb0860e82fbaa0a5063cea82217483c0
chen jian <jian.chen@iim.ltd>
2018-11-19 09:51:31 +0800

    fix typo

--- test_caffe_model.py
+++ test_caffe_model.py
@@ -9,53 +9,37 @@ except ImportError:
 import numpy as np
 import cv2
 
-def compare_diff_sum(tensor1, tensor2):
-    pass
-
-def compare_cosin_dist(tensor1, tensor2):
-    pass
-
-def softmax(x):
-    """Compute softmax values for each sets of scores in x."""
-    e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum()
-
 '''
 Function:
-	apply NMS(non-maximum suppression) on ROIs in same scale(matrix version)
+	apply NMS(non-maximum suppression) on ROIs in same scale
 Input:
-	rectangles: rectangles[i][0:3] is the position, rectangles[i][4] is score
+	rectangles: rectangles[i][0:3] is the position, rectangles[i][4] is scale, rectangles[i][5] is score
 Output:
 	rectangles: same as input
 '''
 def NMS(rectangles,threshold,type):
-    if len(rectangles)==0:
-	return rectangles
-    boxes = np.array(rectangles)
-    x1 = boxes[:,0]
-    y1 = boxes[:,1]
-    x2 = boxes[:,2]
-    y2 = boxes[:,3]
-    s  = boxes[:,4]
-    area = np.multiply(x2-x1+1, y2-y1+1)
-    I = np.array(s.argsort())
-    pick = []
-    while len(I)>0:
-	xx1 = np.maximum(x1[I[-1]], x1[I[0:-1]]) #I[-1] have hightest prob score, I[0:-1]->others
-        yy1 = np.maximum(y1[I[-1]], y1[I[0:-1]])
-        xx2 = np.minimum(x2[I[-1]], x2[I[0:-1]])
-        yy2 = np.minimum(y2[I[-1]], y2[I[0:-1]])
-	w = np.maximum(0.0, xx2 - xx1 + 1)
-        h = np.maximum(0.0, yy2 - yy1 + 1)
-        inter = w * h
-	if type == 'iom':
-	    o = inter / np.minimum(area[I[-1]], area[I[0:-1]])
-	else:
-	    o = inter / (area[I[-1]] + area[I[0:-1]] - inter)
-	pick.append(I[-1])
-	I = I[np.where(o<=threshold)[0]]
-    result_rectangle = boxes[pick].tolist()
-    return result_rectangle
+    sorted(rectangles,key=itemgetter(4),reverse=True)
+    result_rectangles = rectangles
+    number_of_rects = len(result_rectangles)
+    cur_rect = 0
+    while cur_rect < number_of_rects : 
+        rects_to_compare = number_of_rects - cur_rect - 1 
+        cur_rect_to_compare = cur_rect + 1 
+        while rects_to_compare > 0:
+	    score = 0
+	    if type == 'iou':
+		score =  IoU(result_rectangles[cur_rect], result_rectangles[cur_rect_to_compare])
+	    else:
+		score =  IoM(result_rectangles[cur_rect], result_rectangles[cur_rect_to_compare])
+            if score >= threshold:
+                del result_rectangles[cur_rect_to_compare]      # delete the rectangle
+                number_of_rects -= 1
+            else:
+                cur_rect_to_compare += 1    # skip to next rectangle            
+            rects_to_compare -= 1
+        cur_rect += 1   # finished comparing for current rectangle
+    return result_rectangles
+
 '''
 Function:
 	Filter face position and calibrate bounding box on 12net's output
@@ -71,47 +55,42 @@ Output:
 	rectangles: face positions and landmarks
 '''
 def filter_face_48net(cls_prob,roi,pts,rectangles,width,height,threshold):
-    prob = cls_prob[:,1]
-    pick = np.where(prob>=threshold)
-    rectangles = np.array(rectangles)
-    x1  = rectangles[pick,0]
-    y1  = rectangles[pick,1]
-    x2  = rectangles[pick,2]
-    y2  = rectangles[pick,3]
-    sc  = np.array([prob[pick]]).T
-    dx1 = roi[pick,0]
-    dx2 = roi[pick,1]
-    dx3 = roi[pick,2]
-    dx4 = roi[pick,3]
-    w   = x2-x1
-    h   = y2-y1
-    pts0= np.array([(w*pts[pick,0]+x1)[0]]).T
-    pts1= np.array([(h*pts[pick,5]+y1)[0]]).T
-    pts2= np.array([(w*pts[pick,1]+x1)[0]]).T
-    pts3= np.array([(h*pts[pick,6]+y1)[0]]).T
-    pts4= np.array([(w*pts[pick,2]+x1)[0]]).T
-    pts5= np.array([(h*pts[pick,7]+y1)[0]]).T
-    pts6= np.array([(w*pts[pick,3]+x1)[0]]).T
-    pts7= np.array([(h*pts[pick,8]+y1)[0]]).T
-    pts8= np.array([(w*pts[pick,4]+x1)[0]]).T
-    pts9= np.array([(h*pts[pick,9]+y1)[0]]).T
-    x1  = np.array([(x1+dx1*w)[0]]).T
-    y1  = np.array([(y1+dx2*h)[0]]).T
-    x2  = np.array([(x2+dx3*w)[0]]).T
-    y2  = np.array([(y2+dx4*h)[0]]).T
-    rectangles=np.concatenate((x1,y1,x2,y2,sc,pts0,pts1,pts2,pts3,pts4,pts5,pts6,pts7,pts8,pts9),axis=1)
-    pick = []
-    for i in range(len(rectangles)):
-	x1 = int(max(0     ,rectangles[i][0]))
-	y1 = int(max(0     ,rectangles[i][1]))
-	x2 = int(min(width ,rectangles[i][2]))
-	y2 = int(min(height,rectangles[i][3]))
-	if x2>x1 and y2>y1:
-	    pick.append([x1,y1,x2,y2,rectangles[i][4],
-			 rectangles[i][5],rectangles[i][6],rectangles[i][7],rectangles[i][8],rectangles[i][9],rectangles[i][10],rectangles[i][11],rectangles[i][12],rectangles[i][13],rectangles[i][14]])
-    return NMS(pick,0.7,'iom')
+    boundingBox = []
+    rect_num = len(rectangles)
+    for i in range(rect_num):
+	if cls_prob[i][1]>threshold:
+	    rect = [rectangles[i][0],rectangles[i][1],rectangles[i][2],rectangles[i][3],cls_prob[i][1],
+		   roi[i][0],roi[i][1],roi[i][2],roi[i][3],
+		   pts[i][0],pts[i][5],pts[i][1],pts[i][6],pts[i][2],pts[i][7],pts[i][3],pts[i][8],pts[i][4],pts[i][9]]
+	    boundingBox.append(rect)
+    rectangles = NMS(boundingBox,0.7,'iom')
+    rect = []
+    
+    for rectangle in rectangles:
+	roi_w = rectangle[2]-rectangle[0]+1
+	roi_h = rectangle[3]-rectangle[1]+1
 
-def compare_models(prefix_caffe, size):
+  	x1 = round(max(0     , rectangle[0]+rectangle[5]*roi_w))
+        y1 = round(max(0     , rectangle[1]+rectangle[6]*roi_h))
+        x2 = round(min(width , rectangle[2]+rectangle[7]*roi_w))
+        y2 = round(min(height, rectangle[3]+rectangle[8]*roi_h))
+	pt0 = rectangle[ 9]*roi_w + rectangle[0] -1
+	pt1 = rectangle[10]*roi_h + rectangle[1] -1
+	pt2 = rectangle[11]*roi_w + rectangle[0] -1
+	pt3 = rectangle[12]*roi_h + rectangle[1] -1
+	pt4 = rectangle[13]*roi_w + rectangle[0] -1
+	pt5 = rectangle[14]*roi_h + rectangle[1] -1
+	pt6 = rectangle[15]*roi_w + rectangle[0] -1
+	pt7 = rectangle[16]*roi_h + rectangle[1] -1
+	pt8 = rectangle[17]*roi_w + rectangle[0] -1
+	pt9 = rectangle[18]*roi_h + rectangle[1] -1
+	score = rectangle[4]
+	rect_ = np.round([x1,y1,x2,y2,pt0,pt1,pt2,pt3,pt4,pt5,pt6,pt7,pt8,pt9]).astype(int)
+	rect_= np.append(rect_,score)
+	rect.append(rect_)
+    return rect
+
+def compare_models(prefix_caffe):
     threshold = [0.6,0.6,0.7]
     deploy = prefix_caffe + '48net.prototxt'
     caffemodel = prefix_caffe + '48net.caffemodel'
@@ -141,9 +120,9 @@ def compare_models(prefix_caffe, size):
     print "done"
     draw = cv2.imread('face1.jpg')
     for rectangle in rectangles:
-        cv2.putText(draw,str(rectangle[4]),(int(rectangle[0]),int(rectangle[1])),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0))
+        cv2.putText(draw,str(rectangle[14]),(int(rectangle[0]),int(rectangle[1])),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0))
         cv2.rectangle(draw,(int(rectangle[0]),int(rectangle[1])),(int(rectangle[2]),int(rectangle[3])),(255,0,0),1)
-    for i in range(5,15,2):
+    for i in range(4,14,2):
     	cv2.circle(draw,(int(rectangle[i+0]),int(rectangle[i+1])),2,(0,255,0))
     cv2.imshow("test",draw)
     cv2.waitKey()
@@ -152,5 +131,4 @@ def compare_models(prefix_caffe, size):
     
 if __name__ == "__main__":
     prefix_caffe = "/var/darknet/MXNet2Caffe/model_caffe/"
-    size = (1, 3, 112, 112)
-    compare_models(prefix_caffe, size)
+    compare_models(prefix_caffe)
